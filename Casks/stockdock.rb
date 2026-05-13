4b5bad9d7b4819ada6090444d3bc80812b967ddf
simonsruggi <simone.ruggiero97@gmail.com>
2026-05-13 15:46:46 +0200

    Add StockDock cask v1.0

---
+++ Casks/stockdock.rb
@@ -0,0 +1,19 @@
+cask "stockdock" do
+  version "1.0"
+  sha256 "37ef0bdb5b02501e538fc833ad84a55c5af7c866d190db2e0c4cbd05b65b9081"
+
+  url "https://github.com/simonsruggi/StockDock/releases/download/v#{version}/StockDock.zip"
+  name "StockDock"
+  desc "Free macOS menu bar app to track stocks and portfolios in real time"
+  homepage "https://github.com/simonsruggi/StockDock"
+
+  depends_on macos: ">= :sonoma"
+
+  app "StockDock.app"
+
+  zap trash: [
+    "~/Library/Application Support/StockDock",
+    "~/Library/Caches/com.simone.stockdock",
+    "~/Library/Preferences/com.simone.stockdock.plist",
+  ]
+end
