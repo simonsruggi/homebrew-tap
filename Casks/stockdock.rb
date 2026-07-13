adaa69a4bae8c93169a0bb7b38181f0d9d76a19f
simonsruggi <simone.ruggiero97@gmail.com>
2026-07-13 13:50:25 +0200

    stockdock 1.8.1

--- Casks/stockdock.rb
+++ Casks/stockdock.rb
@@ -1,6 +1,6 @@
 cask "stockdock" do
-  version "1.8.0"
-  sha256 "5b19ee2276389a32df56adca0fe4f306daee9a60b039abc9fa268389a022a31a"
+  version "1.8.1"
+  sha256 "a716b2d683acdaa7db8d36466a90e91449fb25d0f85873947740dbc3c233730e"
 
   url "https://github.com/simonsruggi/StockDock/releases/download/v#{version}/StockDock.zip"
   name "StockDock"
